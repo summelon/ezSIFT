f690128f0d2c6a99edac4eb96c0d31bb6614a766
summelon <summelon7@gamil.com>
2020-12-18 19:00:05 +0800

    Update row_filter_transpose write buffer in host & flat nested loop in two global func

--- src/kernel.cu
+++ src/kernel.cu
@@ -7,8 +7,10 @@
 #include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <algorithm>
 
-#define TILE_SIZE 64
+#define HEIGHT_TILE 8
+#define WIDTH_TILE 8
 
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
@@ -22,29 +24,38 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 }
 
 
+__global__ void cal_width_elem(int w, int h, int height_now, int gR,
+                             float *coef, float *dev_buf, float *dev_dst)
+{
+    int width_now = blockIdx.x * blockDim.x + threadIdx.x;
+    if (width_now >= w) return;
+
+    float *prow = dev_buf + height_now * (w + gR * 2) + width_now;
+    float *dstData = dev_dst + height_now + width_now * h;
+    float partialSum = 0.0f;
+
+    for (int i = -gR; i <= gR; i++) {
+        partialSum += (*coef++) * (*prow++);
+    }
+
+    // prow -= 2 * gR;
+    *dstData = partialSum;
+}
+
 __global__ void row_gauss_kernel(
         int w, int h, int gR,
-        float *dev_src, float *dev_dst, float *dev_coef,
-        float *dev_buf)
+        float *dev_dst, float *dev_coef, float *dev_buf)
 {
     int height_now = blockIdx.x * blockDim.x + threadIdx.x;
     if (height_now >= h) return;
 
+    // int dimBlock = WIDTH_TILE;
+    // int dimGrid = w / WIDTH_TILE + 1;
+    // cal_width_elem<<<dimGrid, dimBlock>>>(w, h, height_now, gR, dev_coef, dev_buf, dev_dst);
+
     float partialSum;
     float *coef;
-    // float *row_buf = new float[w + gR * 2];
-    float *row_buf = dev_buf + height_now * (w + gR * 2);
-    float *row_start = dev_src + height_now * w;
-    memcpy(row_buf + gR, row_start, sizeof(float) * w);
-    float firstData = *(row_start);
-    float lastData = *(row_start + w - 1);
-
-    for (int i = 0; i < gR; i++) {
-        row_buf[i] = firstData;
-        row_buf[i + w + gR] = lastData;
-    }
-
-    float *prow = row_buf;
+    float *prow = dev_buf + height_now * (w + gR * 2);
     float *dstData = dev_dst + height_now;
     for (int c = 0; c < w; c++) {
         partialSum = 0.0f;
@@ -58,39 +69,46 @@ __global__ void row_gauss_kernel(
         *dstData = partialSum;
         dstData += h;
     }
-
-    // delete [] row_buf;
-    // row_buf = nullptr;
 }
 
 
 void row_filter_transpose_gpu(
-        float *src, float *dst, int w, int h,
+        float *host_src, float *host_dst, int w, int h,
         float *coef1d, int gR)
 {
-    // Allocate and copy to device memory
-    float *dev_src, *dev_dst, *dev_coef;
-    float *dev_buf;
     int img_size = w * h;
     int buf_size = h * (w + gR * 2);
+    // Allocate and copy to device memory
+    float *dev_buf, *dev_dst, *dev_coef;
+    float *row_buf, *row_start;
+    float *host_buf = new float[buf_size];
 
-    gpuErrchk( cudaMalloc( (void**)&dev_src, sizeof(float)*img_size ) );
+    for (int height = 0; height < h; height++)
+    {
+        row_buf = host_buf + height * (w + gR * 2);
+        row_start = host_src + height * w;
+        memcpy(row_buf+gR, row_start, sizeof(float)*w);
+        std::fill(row_buf, row_buf+gR, *row_start);
+        std::fill( row_buf+w+gR, row_buf+w+2*gR, *(row_start+w-1) );
+    }
+
     gpuErrchk( cudaMalloc( (void**)&dev_dst, sizeof(float)*img_size ) );
     gpuErrchk( cudaMalloc( (void**)&dev_coef, sizeof(float)*2*gR ) );
     gpuErrchk( cudaMalloc( (void**)&dev_buf, sizeof(float)*buf_size) );
-    gpuErrchk( cudaMemcpy(dev_src, src, sizeof(float)*img_size, cudaMemcpyHostToDevice) );
+    gpuErrchk( cudaMemcpy(dev_buf, host_buf, sizeof(float)*buf_size, cudaMemcpyHostToDevice) );
     gpuErrchk( cudaMemcpy(dev_coef, coef1d, sizeof(float)*2*gR, cudaMemcpyHostToDevice) );
 
     // Set block and grid size
-    int dimBlock = TILE_SIZE;
-    int dimGrid = h/TILE_SIZE+1;
+    int dimBlock = HEIGHT_TILE;
+    int dimGrid = h / HEIGHT_TILE + 1;
 
     // Run kernel function
-    row_gauss_kernel<<<dimGrid, dimBlock>>>(w, h, gR, dev_src, dev_dst, dev_coef, dev_buf);
+    row_gauss_kernel<<<dimGrid, dimBlock>>>(w, h, gR, dev_dst, dev_coef, dev_buf);
     gpuErrchk( cudaPeekAtLastError() );
 
     // Transfer from device mem to host and Clean
-    cudaMemcpy(dst, dev_dst, sizeof(float)*img_size, cudaMemcpyDeviceToHost);
-    cudaFree(dev_src); cudaFree(dev_dst); cudaFree(dev_coef);
-    cudaFree(dev_buf);
+    gpuErrchk( cudaMemcpy(host_dst, dev_dst, sizeof(float)*img_size, cudaMemcpyDeviceToHost) );
+    cudaFree(dev_dst); cudaFree(dev_coef); cudaFree(dev_buf);
+    delete [] host_buf;
+
 }
